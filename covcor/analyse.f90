03c8bdaf17fc2d9220b4be3d22bb1583210a76fa
enderswype <enderswype@gmail.com>
2020-04-30 15:48:26 +0200

    visualisation ok

--- covcor/analyse.f90
+++ covcor/analyse.f90
@@ -1,6 +1,8 @@
 PROGRAM MAIN
 
-  integer, parameter :: height=141970, width=77
+  integer, parameter :: height=141900, width=77
+  real(kind=8), dimension(100,width) :: tmp
+  integer, dimension(width-3) :: vals
   real(kind=8), dimension(width) :: tmp_RA
   integer :: zero=0, small=0, supone=0, total=0
 
@@ -18,11 +20,38 @@ PROGRAM MAIN
       end if
     end do
   end do
+close(1)
+
+open(1, file="../../transfert/RA.out", status='old', action='read')
+open(2, file="./zero.ppm", status="new", action="write")
+write(2,'(a)')"P2"
+write(2,'(a)')"74 14190"
+write(2,'(a)')"100"
+
+do i = 1, height, 100
+  do j = 1, 100
+    read(1, *) tmp(j, :)
+  end do
+  
+  vals = 0
+  do x = 4, width
+    do y = 1, 100
+      if (tmp(y, x) == 0) vals(y) = vals(y) + 1
+    end do
+  enddo
+
+  do x = 1, 100
+    write(2, *) vals(x)
+  enddo
+
+end do
+
+close(1)
+close(2)
+
 print*, "zero   small   supone   total-small&zero   total"
 print*, zero, small, supone, total - small - zero, total
 print*, real(zero)/real(total)*100, real(small)/real(total)*100, &
   real(supone)/real(total)*100, real(total - small - zero)/real(total)*100, "100%"
-
-  open(2, file="../../transfert/fort.299", status='old', action='read')
 
 END PROGRAM MAIN
--- mini_tests/ppm.f90
+++ mini_tests/ppm.f90
@@ -2,9 +2,9 @@ program main
 
 
   open(1, file='./img.ppm', status="new", action="write")
-  write(1, *) "P6"
-  write(1, *) "5  5"
-  write(1,*) "255"
+  write(1, '(a)')"P6"
+  write(1, *)"5  5"
+  write(1,*)"255"
   do i=1, 25
     write(1, *) i*10
   end do
